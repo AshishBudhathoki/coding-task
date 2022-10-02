be6fdf4ab51e527d9c3e9824ca1acf45b9454e25
ashish <ashish.budhathoki@outlook.com>
2022-10-02 20:58:48 +1100

    create weatherListingState

---
+++ weather/weather_presentation/src/main/java/com/ashish/weather_presentation/weather_listings/WeatherListingsState.kt
@@ -0,0 +1,9 @@
+package com.ashish.weather_presentation.weather_listings
+
+import com.ashish.weather_domain.model.WeatherData
+
+data class WeatherListingsState(
+    val weatherData: List<WeatherData> = emptyList(),
+    val isLoading: Boolean = false,
+    val isRefreshing: Boolean = false
+)
