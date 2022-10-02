8f11402ab54940307e3995c5bec5f580047faefd
ashish <ashish.budhathoki@outlook.com>
2022-10-02 21:25:37 +1100

    create core ui layer

---
+++ core-ui/src/main/java/com/ashish/core_ui/Color.kt
@@ -0,0 +1,10 @@
+package com.ashish.core_ui
+
+import androidx.compose.ui.graphics.Color
+
+val LightGray = Color(0xFF808080)
+val MediumGray = Color(0xFF404040)
+val DarkGray = Color(0xFF202020)
+val TextWhite = Color(0xFFEEEEEE)
+val HeaderColor = Color(0xFF224154)
+val TabIndicatorColor = Color(0xFF57CCD4)
