ea63b039043682a014b00f52ae1356e114f21e81
ashish <ashish.budhathoki@outlook.com>
2022-10-02 21:36:27 +1100

    add core-ui layer in weather presentation

--- buildSrc/src/main/java/Modules.kt
+++ buildSrc/src/main/java/Modules.kt
@@ -2,6 +2,7 @@ object Modules {
     const val app = ":app"
 
     const val core = ":core"
+    const val coreUi = ":core-ui"
 
     const val weatherData = ":weather:weather_data"
     const val weatherDomain = ":weather:weather_domain"
