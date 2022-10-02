5eaf9d793f8a40bbf39d11bcec4847f0ed5d601a
ashish <ashish.budhathoki@outlook.com>
2022-10-02 22:55:48 +1100

    add di for GetWeatherItemUseCase

--- weather/weather_domain/src/main/java/com/ashish/weather_domain/di/WeatherDomainUseCase.kt
+++ weather/weather_domain/src/main/java/com/ashish/weather_domain/di/WeatherDomainUseCase.kt
@@ -2,12 +2,16 @@ package com.ashish.weather_domain.di
 
 import com.ashish.weather_domain.repository.WeatherRepository
 import com.ashish.weather_domain.usecase.GetWeatherDataUseCase
+import com.ashish.weather_domain.usecase.GetWeatherItemUseCase
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.components.ViewModelComponent
 import dagger.hilt.android.scopes.ViewModelScoped
 
+/***
+ * Dependencies for usecases from domain
+ */
 @Module
 @InstallIn(ViewModelComponent::class)
 object WeatherDomainUseCase {
@@ -18,5 +22,13 @@ object WeatherDomainUseCase {
         repository: WeatherRepository
     ): GetWeatherDataUseCase {
         return GetWeatherDataUseCase(repository)
+    }
+
+    @ViewModelScoped
+    @Provides
+    fun provideWeatherDataItemUseCase(
+        repository: WeatherRepository
+    ): GetWeatherItemUseCase {
+        return GetWeatherItemUseCase(repository)
     }
 }
