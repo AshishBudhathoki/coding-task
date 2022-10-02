de39546426bc4fda20ad08de23895280647b045d
ashish <ashish.budhathoki@outlook.com>
2022-10-02 23:17:01 +1100

    fix null crash in weather item clicked

--- core/src/main/java/com/ashish/core/util/Extensions.kt
+++ core/src/main/java/com/ashish/core/util/Extensions.kt
@@ -6,10 +6,17 @@ import java.util.*
 /***
  * Converts date to required string format
  */
-fun String.toDateString(): String = SimpleDateFormat(
-    "hh:mm a dd/MMMM/yyyy",
-    Locale.ENGLISH
-).format(this?.toLong()?.times(1000)?.let { Date(it) })
-    .replace("AM", "am")
-    .replace("PM", "pm")
-    .replace("/", " ")
+fun String.toDateString(): String {
+    return if (this != null && this != "null") {
+        SimpleDateFormat(
+            "hh:mm a dd/MMMM/yyyy",
+            Locale.ENGLISH
+        ).format(this.toLong().times(1000)?.let { Date(it) })
+            .replace("AM", "am")
+            .replace("PM", "pm")
+            .replace("/", " ")
+    } else {
+        ""
+    }
+
+}
