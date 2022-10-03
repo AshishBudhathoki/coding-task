a3f79c7498d902f724d4fdf10bf500bb1f0e9ab5
ashish <ashish.budhathoki@outlook.com>
2022-10-04 09:47:04 +1100

    add dependencies for navigation test

--- buildSrc/src/main/java/Testing.kt
+++ buildSrc/src/main/java/Testing.kt
@@ -27,4 +27,7 @@ object Testing {
 
     private const val testRunnerVersion = "1.4.0"
     const val testRunner = "androidx.test:runner:$testRunnerVersion"
+
+    private const val navigationTestVersion = "2.3.5"
+    const val navigationTest = "androidx.navigation:navigation-testing:$navigationTestVersion"
 }
